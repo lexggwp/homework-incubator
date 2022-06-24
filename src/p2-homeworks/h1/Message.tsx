d3a4134e322a8ce7768990b052f98f71e368df6c
lexggwp <alexeyfilipp@gmail.com>
2022-06-24 15:38:50 +0300

    feat: added static chat window

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import c from './Message.module.css';
+
+type MessagePropsType = {
+    avatar: string;
+    name: string;
+    message: string;
+    time: string;
+}
+
+function Message(props: MessagePropsType) {
+    return (
+        <div className={c.message}>
+            <div className={c.message__avatar}>
+                <img src={props.avatar} alt=""/>
+            </div>
+            <div className={c.message__block}>
+                <div className={c.message__name}>{props.name}</div>
+                <div className={c.message__text}>{props.message}
+                    <div className={c.message__time}>{props.time}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Message

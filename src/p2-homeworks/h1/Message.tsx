55bb55f4ec496eeadb6a22d966b8813283acb008
lexggwp <alexeyfilipp@gmail.com>
2022-07-05 11:27:47 +0300

    fixed: new style.css

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -14,11 +14,12 @@ function Message(props: MessagePropsType) {
             <div className={c.message__avatar}>
                 <img src={props.avatar} alt=""/>
             </div>
+
+
             <div className={c.message__block}>
                 <div className={c.message__name}>{props.name}</div>
-                <div className={c.message__text}>{props.message}
-                    <div className={c.message__time}>{props.time}</div>
-                </div>
+                <div className={c.message__text}>{props.message}</div>
+                <div className={c.message__time}>{props.time}</div>
             </div>
         </div>
     )

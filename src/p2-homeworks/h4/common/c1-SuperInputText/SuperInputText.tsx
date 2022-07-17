c10fc1284057c4e65b588728fe6f1a7e74046b5a
lexggwp <alexeyfilipp@gmail.com>
2022-07-17 14:30:39 +0300

    refactor: hw4

--- src/p2-homeworks/h4/common/c1-SuperInputText/SuperInputText.tsx
+++ src/p2-homeworks/h4/common/c1-SuperInputText/SuperInputText.tsx
@@ -39,14 +39,14 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
     }
 
     const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ''}`
-    const finalInputClassName = `${s.errorInput} ${className}` // need to fix with (?:) and s.superInput
+    const finalInputClassName = `${s.input} ${error ? s.errorInput : s.superInput} ${className}` // need to fix with (?:) and s.superInput
 
     return (
         <>
             <input
                 type={'text'}
                 onChange={onChangeCallback}
-                onKeyPress={onKeyPressCallback}
+                onKeyDown={onKeyPressCallback}
                 className={finalInputClassName}
 
                 {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
